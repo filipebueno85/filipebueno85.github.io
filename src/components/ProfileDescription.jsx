24e4e9df2570616e2b1c8940eaf17a71555bc96b
Filipe Bueno <filipe_bueno85@live.com>
2023-01-30 14:47:51 -0300

    alterando bkg e algumas informações

--- src/components/ProfileDescription.jsx
+++ src/components/ProfileDescription.jsx
@@ -7,7 +7,7 @@ class ProfileDescription extends Component {
           <p>Sou formado em Negócios da Moda, passei por vários setores na minha jornada profissional.
           Minha primeira experiencia foi em uma emissora de televisão onde atuei como assistente de figurino.
           Já atuei no varejo como consultor em vendas, onde adquiri muito conhecimento sobre comportamento de pessoas e como trabalhar em equipe.
-          Passei também pela área de marketing digital em marcas e agencias de publicidade.
+          Atuei também em marketing digital em marcas e agencias de publicidade.
           Realizei trabalhos de design gráfico, na criação de logotipos e campanhas publicitarias.
           Sempre gostei de tecnologia e sempre tive muita curiosidade de como funcionaria a fundo.
           Atualmente estou estudando Desenvolvimento Web, para trabalhar no mercado de tecnologia, onde realmente estou me reencontrando novamente.

3049a4755ad3ead9c74635fab7760dc85e04113d
Filipe Bueno <filipe_bueno85@live.com>
2023-02-09 10:28:31 -0300

    atualizando projetos

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,10 +13,53 @@ class Projects extends Component {
         <div className="projetos">
           <div className="projeto-container">
             <img
+              src={onlineStore}
+              alt="onlineStore"
+            />
+            <p>A aplicação é uma simulação de um e-commerce, sendo possível realizar um produto específico e também filtrando por categorias. Você pode adicionar e remover produtos do carrinho de compras. Clique no link abaixo para acessar a aplicação.</p>
+            <div className="habilidades-container-project">
+            <div className="logos">
+              <img style={ { width:  '80px' } } src="https://logospng.org/download/javascript/logo-javascript-icon-256.png" alt="javascript" />
+              <img style={ { width:  '80px' } } src="https://logospng.org/download/css-3/logo-css-3-256.png" alt="css" />
+              <img style={ { width:  '80px' } } src="https://logospng.org/download/html-5/logo-html-5-256.png" alt="html" />
+              <img style={ { width:  '80px' } } src="https://logospng.org/download/react/logo-react-256.png" alt="react" />
+            </div>
+            </div>
+            <br />
+            <a className="trunfo-link" href="https://online-store-group13.vercel.app/" target="_blank" rel="noreferrer">Online Store</a>
+          </div>
+          <div className="projeto-container">
+            <img
+              src={carteiraDigital}
+              alt="Carteira Digital"
+            />
+            <p>Mais um desafio construir essa aplicação, usando React e Redux, a aplicação trata-se de uma carteira digital. Você consegue adicionar despesas e converter o valor para outra moeda, sendo possível editar essa despesa e também excluir.</p>
+            <div className="habilidades-container-project">
+            <div className="logos">
+              <img style={ { width:  '80px' } } src="https://logospng.org/download/javascript/logo-javascript-icon-256.png" alt="javascript" />
+              <img style={ { width:  '80px' } } src="https://logospng.org/download/css-3/logo-css-3-256.png" alt="css" />
+              <img style={ { width:  '80px' } } src="https://logospng.org/download/html-5/logo-html-5-256.png" alt="html" />
+              <img style={ { width:  '80px' } } src="https://logospng.org/download/react/logo-react-256.png" alt="react" />
+              <img style={ { width:  '80px' } } src="https://cdn.worldvectorlogo.com/logos/redux.svg" alt="react" />
+            </div>
+            </div>
+            <br />
+            <a className="trunfo-link" href="https://carteira-digital-filipebueno85.vercel.app/" target="_blank" rel="noreferrer">Carteira Digital</a>
+          </div>
+          <div className="projeto-container">
+            <img
               src={cardTrunfo}
               alt="card-trunfo"
             />
             <p>A aplicação trata-se de um jogo tipo SuperTrunfo, você pode criar seu próprio baralho de acordo com seus personagens favoritos!</p>
+            <div className="habilidades-container-project">
+            <div className="logos">
+              <img style={ { width:  '80px' } } src="https://logospng.org/download/javascript/logo-javascript-icon-256.png" alt="javascript" />
+              <img style={ { width:  '80px' } } src="https://logospng.org/download/css-3/logo-css-3-256.png" alt="css" />
+              <img style={ { width:  '80px' } } src="https://logospng.org/download/html-5/logo-html-5-256.png" alt="html" />
+              <img style={ { width:  '80px' } } src="https://logospng.org/download/react/logo-react-256.png" alt="react" />
+            </div>
+            </div>
             <br />
             <a className="trunfo-link" href="https://card-trunfo-p55g.vercel.app/" target="_blank" rel="noreferrer">Card Trunfo</a>
           </div>
@@ -26,6 +69,14 @@ class Projects extends Component {
               alt="card-trunfo"
             />
             <p>No Filipe Tunes você consegue buscar seu artista favorito, escutar o trecho de de uma faixa musical, sendo possível favoritar uma música!</p>
+            <div className="habilidades-container-project">
+            <div className="logos">
+              <img style={ { width:  '80px' } } src="https://logospng.org/download/javascript/logo-javascript-icon-256.png" alt="javascript" />
+              <img style={ { width:  '80px' } } src="https://logospng.org/download/css-3/logo-css-3-256.png" alt="css" />
+              <img style={ { width:  '80px' } } src="https://logospng.org/download/html-5/logo-html-5-256.png" alt="html" />
+              <img style={ { width:  '80px' } } src="https://logospng.org/download/react/logo-react-256.png" alt="react" />
+            </div>
+            </div>
             <br />
             <a className="trunfo-link" href="https://filipe-tunes.vercel.app/" target="_blank" rel="noreferrer">Filipe Tunes</a>
           </div>
@@ -39,6 +90,14 @@ class Projects extends Component {
               O resultado da pesquisa contém filmes, series, games que o personagem aparece.
 
               O conteúdo é de uma API com personagens da Disney.</p>
+              <div className="habilidades-container-project">
+            <div className="logos">
+              <img style={ { width:  '80px' } } src="https://logospng.org/download/javascript/logo-javascript-icon-256.png" alt="javascript" />
+              <img style={ { width:  '80px' } } src="https://logospng.org/download/css-3/logo-css-3-256.png" alt="css" />
+              <img style={ { width:  '80px' } } src="https://logospng.org/download/html-5/logo-html-5-256.png" alt="html" />
+              <img style={ { width:  '80px' } } src="https://logospng.org/download/react/logo-react-256.png" alt="react" />
+            </div>
+            </div>
             <br />
             <a className="trunfo-link" href="https://wiki-disney.vercel.app/" target="_blank" rel="noreferrer">WikiDisney</a>
           </div>
@@ -47,28 +106,19 @@ class Projects extends Component {
               src={PrevTempo}
               alt="PrevTempo"
             />
-            <br />
             <p>No PrevTempo você consegue pesquisar qualquer cidade do Brasil, para saber a previsão do dia e podendo ver para os próximos 10 dias.</p>
+            <div className="habilidades-container-project">
+            <div className="logos">
+              <img style={ { width:  '80px' } } src="https://logospng.org/download/javascript/logo-javascript-icon-256.png" alt="javascript" />
+              <img style={ { width:  '80px' } } src="https://logospng.org/download/css-3/logo-css-3-256.png" alt="css" />
+              <img style={ { width:  '80px' } } src="https://logospng.org/download/html-5/logo-html-5-256.png" alt="html" />
+              <img style={ { width:  '80px' } } src="https://logospng.org/download/react/logo-react-256.png" alt="react" />
+            </div>
+            </div>
+            <br />
             <a className="trunfo-link" href="https://previ-tempo.vercel.app/" target="_blank" rel="noreferrer">PrevTempo</a>
           </div>
-          <div className="projeto-container">
-            <img
-              src={onlineStore}
-              alt="onlineStore"
-            />
-            <p>A aplicação é uma simulação de um e-commerce, sendo possível realizar um produto específico e também filtrando por categorias. Você pode adicionar e remover produtos do carrinho de compras. Clique no link abaixo para acessar a aplicação.</p>
-            <br />
-            <a className="trunfo-link" href="https://online-store-group13.vercel.app/" target="_blank" rel="noreferrer">Online Store</a>
-          </div>
-          <div className="projeto-container">
-            <img
-              src={carteiraDigital}
-              alt="Carteira Digital"
-            />
-            <p>Mais um desafio construir essa aplicação, usando React e Redux, a aplicação trata-se de uma carteira digital. Você consegue adicionar despesas e converter o valor para outra moeda, sendo possível editar essa despesa e também excluir.</p>
-            <br />
-            <a className="trunfo-link" href="https://carteira-digital-filipebueno85.vercel.app/" target="_blank" rel="noreferrer">Carteira Digital</a>
-          </div>
+
         </div>
       </div>
     );

245c78d99ac48ce3ae17fffbb97241c1d50db1a5
Filipe Bueno <filipe_bueno85@live.com>
2023-05-02 12:33:27 -0300

    editando a pagina inicial

--- src/components/Habilidades.jsx
+++ src/components/Habilidades.jsx
@@ -8,11 +8,23 @@ class Habilidades extends Component {
         <div className="logos">
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/600px-Octicons-mark-github.svg.png?20180806170715" alt="githublogo" />
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Git-logo.svg/640px-Git-logo.svg.png" alt="git" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Bash_Logo_Colored.svg/2048px-Bash_Logo_Colored.svg.png" alt="bash" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/590px-Node.js_logo.svg.png?20170401104355" alt="nodejs" />
         <img src="https://logospng.org/download/javascript/logo-javascript-icon-256.png" alt="javascript" />
         <img src="https://logospng.org/download/css-3/logo-css-3-256.png" alt="css" />
         <img src="https://logospng.org/download/html-5/logo-html-5-256.png" alt="html" />
         <img src="https://logospng.org/download/react/logo-react-256.png" alt="react" />
         <img src="https://cdn.worldvectorlogo.com/logos/redux.svg" alt="redux" />
+        <img src="https://assets.stickpng.com/images/62a9c7ab8ff6441a2952dad2.png" alt="docker" />
+        <img src="https://desosa.nl/projects/docker-compose/2020/03/02/images/docker-compose/logo_docker_compose.png" alt="docker-compose" />
+        <img src="https://1000logos.net/wp-content/uploads/2020/08/MySQL-Logo.png" alt="mysql" />
+        <img src="https://docs.knapsackpro.com/images/blog/posts/run-jest-on-github-actions-with-parallelization/jest.png" alt="jest" />
+        <img src="https://testing-library.com/img/octopus-128x128.png" alt="testing-library" />
+        <img src="https://camo.githubusercontent.com/58045a79a69afea4cab1cea6def6d911fba3956cf5fd683addf41c032aa64088/68747470733a2f2f636c6475702e636f6d2f78465646784f696f41552e737667" alt="mocha" />
+        <img src="https://avatars.githubusercontent.com/u/1515293?s=280&v=4" alt="chai" />
+        <img src="https://sinonjs.org/assets/images/logo.png" alt="sinon" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/ESLint_logo.svg/1200px-ESLint_logo.svg.png" alt="eslint" />
+        <img src="https://logodownload.org/wp-content/uploads/2022/12/figma-logo-0.png" alt="figma" />
         <img src="https://logosmarcas.net/wp-content/uploads/2020/11/Adobe-Photoshop-Logo-650x366.png" alt="photoshop" />
         <img src="https://logodownload.org/wp-content/uploads/2017/04/adobe-Illustrator-logo-0-1-2048x2048.png" alt="illustrator" />
         <img src="https://www.coreldraw.com/static/cdgs/images/pages/product-info-sheet/key-img.png" alt="coreldraw" />
